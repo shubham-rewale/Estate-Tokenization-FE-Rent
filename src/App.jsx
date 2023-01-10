b61b4dff42b0d57b77aef7e14272cb2ceed6253e
subhajitdas1999 <sd651846@gmail.com>
2023-01-10 11:01:35 +0530

    added Route and new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import Home from "./Home";
+import PropertyDetails from "./PropertyDetils";
+import Proposal from "./Proposal";
+import RentOperationDetails from "./RentOperationDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App">hallo world</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route
+            path="/property/:tokenId"
+            exact
+            element={<PropertyDetails />}
+          />
+          <Route
+            path="/proposals/property/:tokenId"
+            exact
+            element={<Proposal />}
+          />
+          <Route
+            path="/rentOperations/:tokenId"
+            exact
+            element={<RentOperationDetails />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
