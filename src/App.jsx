2fec52bc0eb4c4cbcb53f79245e9950e4078fbb7
subhajitdas1999 <sd651846@gmail.com>
2023-02-16 19:17:21 +0530

    added header component, and userContext and reloadContext for global availability

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,60 @@ import RentOperationDetails from "./RentOperationDetails";
 import ReserveOperationDetails from "./ReserveOperationsDetails";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProposalDetails from "./ProposalDetails";
+import Header from "./Header";
+import { createContext, useState } from "react";
 
+const userContext = createContext();
+const reloadContext = createContext();
 function App() {
+  const [user, setUser] = useState({
+    isConnected: false,
+    address: "",
+    isPropertyManager: "",
+  });
+  const [reloadComponent, setReloadComponent] = useState(true);
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route
-            path="/property/:tokenId"
-            exact
-            element={<PropertyDetails />}
-          />
-          <Route
-            path="/proposals/property/:tokenId"
-            exact
-            element={<Proposal />}
-          />
-          <Route
-            path="/property/:tokenId/proposal/:onChainProposalId"
-            exact
-            element={<ProposalDetails />}
-          />
+        <userContext.Provider value={{ userDetails: [user, setUser] }}>
+          <reloadContext.Provider
+            value={{ reloadDetails: [reloadComponent, setReloadComponent] }}
+          >
+            <Header />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route
+                path="/property/:tokenId"
+                exact
+                element={<PropertyDetails />}
+              />
+              <Route
+                path="/proposals/property/:tokenId"
+                exact
+                element={<Proposal />}
+              />
+              <Route
+                path="/property/:tokenId/proposal/:onChainProposalId"
+                exact
+                element={<ProposalDetails />}
+              />
 
-          <Route
-            path="/rentOperations/:tokenId"
-            exact
-            element={<RentOperationDetails />}
-          />
-          <Route
-            path="/reserveOperations/:tokenId"
-            exact
-            element={<ReserveOperationDetails />}
-          />
-        </Routes>
+              <Route
+                path="/rentOperations/:tokenId"
+                exact
+                element={<RentOperationDetails />}
+              />
+              <Route
+                path="/reserveOperations/:tokenId"
+                exact
+                element={<ReserveOperationDetails />}
+              />
+            </Routes>
+          </reloadContext.Provider>
+        </userContext.Provider>
       </div>
     </Router>
   );
 }
 
-export default App;
+export { App, userContext, reloadContext };
