7aac93d39d54bc55103b1fd7df25bb2eb0a5596e
subhajitdas1999 <sd651846@gmail.com>
2023-02-20 14:06:19 +0530

    added server address from env

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_LOCAL_SERVER_ADDRESS,
+  baseURL: import.meta.env.VITE_SERVER_ADDRESS,
 });
 
 export default AxiosInstance;
